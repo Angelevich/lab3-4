4e4a0b2a7ec06eff0837664c848883f1e1bd48a2
SUPER <angelevich@mail.ru>
2022-04-20 09:53:19 +0300

    Initial commit

---
+++ src/dz_masiv.java
@@ -0,0 +1,11 @@
+public class dz_masiv {
+    public static void main (String[] args) {
+        int[] ms = {2, 3, 4, 5};
+        int z=1;
+        for (int i = 0; i < ms.length; i++) {
+            z= z * ms [i];
+
+            System.out.println(i);
+        }
+    }
+}
